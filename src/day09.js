27f2d9dc1dae17dac3dd8536f0e5aeb5ac7a48db
Alex Jercan <a.jercan@accenture.com>
2022-04-29 17:05:48 +0300

    added day 09

--- src/day09.js
+++ src/day09.js
@@ -1,16 +1,64 @@
 function prepareInput(input) {
-  return input;
+  return input.trim();
 }
 
 function part1(input) {
-    return input;
+  let depth = 0;
+  let score = 0;
+  let garbage = false;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] == "!") {
+      i++;
+      continue;
+    }
+
+    if (garbage) {
+      if (input[i] == ">") {
+        garbage = false;
+      }
+    } else {
+      if (input[i] == "{") {
+        depth += 1;
+      } else if (input[i] == "}") {
+        score += depth;
+        depth -= 1;
+      } else if (input[i] == "<") {
+        garbage = true;
+      }
+    }
+  }
+
+  return score;
 }
 
 function part2(input) {
-    return input;
+    let count = 0;
+    let garbage = false;
+  
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] == "!") {
+        i++;
+        continue;
+      }
+  
+      if (garbage) {
+        if (input[i] == ">") {
+          garbage = false;
+        } else {
+            count++;
+        }
+      } else {
+        if (input[i] == "<") {
+          garbage = true;
+        }
+      }
+    }
+  
+    return count;
 }
 
 export default function solve(input) {
-    input = prepareInput(input);
-    return "Day09\nPart1: " + part1(input) + "\nPart2: " + part2(input) + "\n";
+  input = prepareInput(input);
+  return "Day09\nPart1: " + part1(input) + "\nPart2: " + part2(input) + "\n";
 }
