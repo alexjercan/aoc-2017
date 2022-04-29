8da7a81f8d8625ed3b8fbc2acf21aabd75a66b44
Alex Jercan <a.jercan@accenture.com>
2022-04-29 11:28:59 +0300

    added day 02

--- src/day02.js
+++ src/day02.js
@@ -1,13 +1,26 @@
 function prepareInput(input) {
-  return input;
+    return input.trim().split("\n").map(line => line.trim().split(/\s+/).map(c => parseInt(c)));
 }
 
 function part1(input) {
-    return input;
+    return input.reduce((acc, row) => {
+        return acc + Math.max(...row) - Math.min(...row);
+    }, 0)
 }
 
 function part2(input) {
-    return input;
+    return input.reduce((acc, row) => {
+        let sorted = row.sort((a, b) => a - b);
+        for (let i = 0; i < sorted.length - 1; i++) {
+            for (let j = i + 1; j < sorted.length; j++) {
+                if (sorted[j] % sorted[i] === 0) {
+                    return acc + sorted[j] / sorted[i];
+                }
+            }
+        }
+
+        return 0
+    }, 0)
 }
 
 export default function solve(input) {
