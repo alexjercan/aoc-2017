d237f7b45833f618e514706554b7598c54f09071
Alex Jercan <a.jercan@accenture.com>
2022-05-02 11:31:16 +0300

    added day 10

--- src/day10.js
+++ src/day10.js
@@ -1,16 +1,50 @@
 function prepareInput(input) {
-  return input;
+  return input.split(",").map((n) => parseInt(n));
 }
 
-function part1(input) {
-    return input;
+function hash(list, lengths, i, skip) {
+  const len = list.length;
+  lengths.forEach((v) => {
+    [...Array(v).keys()]
+      .map((k) => list[(k + i) % len])
+      .reverse()
+      .forEach((val, k) => (list[(k + i) % len] = val));
+    i += v + skip;
+    skip++;
+  });
+  return [list, i, skip];
 }
 
-function part2(input) {
-    return input;
+function part1(lengths) {
+  let [list] = hash([...Array(256).keys()], lengths, 0, 0);
+  return list[0] * list[1];
+}
+
+function part2(lengths) {
+  lengths = lengths.split("").map((v) => v.charCodeAt(0));
+  lengths.push(17, 31, 73, 47, 23);
+
+  let list = [...Array(256).keys()];
+  let i = 0;
+  let skip = 0;
+  for (let k = 0; k < 64; ++k) {
+    [list, i, skip] = hash(list, lengths, i, skip);
+  }
+
+  let dense = [];
+  for (let k = 0; k < 16; ++k) {
+    dense.push(list.slice(k * 16, k * 16 + 16).reduce((a, b) => a ^ b));
+  }
+
+  return dense.map((v) => v.toString(16).padStart(2, "0")).join("");
 }
 
 export default function solve(input) {
-    input = prepareInput(input);
-    return "Day10\nPart1: " + part1(input) + "\nPart2: " + part2(input) + "\n";
+  return (
+    "Day10\nPart1: " +
+    part1(prepareInput(input)) +
+    "\nPart2: " +
+    part2(input.trim()) +
+    "\n"
+  );
 }
