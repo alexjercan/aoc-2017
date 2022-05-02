b2a4f78ffd52e8026b09a3f3fbb512688afeb4c7
Alex Jercan <a.jercan@accenture.com>
2022-05-02 15:55:30 +0300

    added day 17

--- src/day17.js
+++ src/day17.js
@@ -1,16 +1,37 @@
 function prepareInput(input) {
-  return input;
+  return parseInt(input.trim());
 }
 
-function part1(input) {
-    return input;
+function part1(n) {
+  const buffer = [0];
+  let pos = 0;
+
+  for (let i = 1; i <= 2017; i++) {
+    pos = (pos + n) % buffer.length;
+    buffer.splice(pos + 1, 0, i);
+    pos++;
+  }
+
+  return buffer[(pos + 1) % buffer.length];
 }
 
-function part2(input) {
-    return input;
+function part2(n) {
+  let size = 1;
+  let pos = 0;
+  let out = 0;
+  for (let i = 1; i <= 50000000; i++) {
+    pos = (pos + n) % size;
+    if (pos == 0) {
+      out = i;
+    }
+    pos++;
+    size++;
+  }
+
+  return out;
 }
 
 export default function solve(input) {
-    input = prepareInput(input);
-    return "Day17\nPart1: " + part1(input) + "\nPart2: " + part2(input) + "\n";
+  input = prepareInput(input);
+  return "Day17\nPart1: " + part1(input) + "\nPart2: " + part2(input) + "\n";
 }
