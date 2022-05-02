7f7053ca90a7f0e30b542d27d1fa6b9cc3f75f27
Alex Jercan <a.jercan@accenture.com>
2022-05-02 12:41:54 +0300

    added day 12

--- src/day12.js
+++ src/day12.js
@@ -1,16 +1,58 @@
 function prepareInput(input) {
-  return input;
+  return input
+    .trim()
+    .split("\n")
+    .reduce((dict, curr) => {
+      const [idx, list] = curr.split(" <-> ");
+      dict.set(
+        parseInt(idx),
+        list.split(", ").map((x) => parseInt(x))
+      );
+      return dict;
+    }, new Map());
 }
 
-function part1(input) {
-    return input;
+function part1(graph) {
+  const start = 0;
+  const visited = new Set();
+  const queue = [start];
+  let count = 0;
+
+  while (queue.length > 0) {
+    const curr = queue.shift();
+    if (visited.has(curr)) continue;
+    visited.add(curr);
+    count++;
+    graph.get(curr).forEach((x) => queue.push(x));
+  }
+
+  return count;
 }
 
 function part2(input) {
-    return input;
+  const notVisited = new Set(input.keys());
+  let groups = 0;
+
+  while (notVisited.size > 0) {
+    const start = notVisited.values().next().value;
+    const visited = new Set();
+    const queue = [start];
+
+    while (queue.length > 0) {
+      const curr = queue.shift();
+      if (visited.has(curr)) continue;
+      visited.add(curr);
+      input.get(curr).forEach((x) => queue.push(x));
+    }
+
+    groups++;
+    visited.forEach((x) => notVisited.delete(x));
+  }
+
+  return groups;
 }
 
 export default function solve(input) {
-    input = prepareInput(input);
-    return "Day12\nPart1: " + part1(input) + "\nPart2: " + part2(input) + "\n";
+  input = prepareInput(input);
+  return "Day12\nPart1: " + part1(input) + "\nPart2: " + part2(input) + "\n";
 }
