1ceea626fb844150881e1c01c0a6bbfc7137eaa1
Alex Jercan <a.jercan@accenture.com>
2022-05-05 11:02:07 +0300

    added day 25

--- src/day25.js
+++ src/day25.js
@@ -1,16 +1,68 @@
+function parseState(state) {
+  const lines = state.split("\n");
+
+  const name = lines[0].split(" ")[2].split(":")[0];
+  const rules = {};
+
+  for (let i = 1; i < lines.length; i += 4) {
+    const rule = parseInt(lines[i].trim().split(" ")[5].split(":")[0]);
+    const write = parseInt(lines[i + 1].trim().split(" ")[4].split(".")[0]);
+    let move = 0;
+    const moveStr = lines[i + 2].trim().split(" ")[6].split(".")[0];
+    if (moveStr === "left") {
+        move = -1;
+    } else if (moveStr === "right") {
+        move = 1;
+    }
+    const state = lines[i + 3].trim().split(" ")[4].split(".")[0];
+    rules[rule] = { write, move, state };
+  }
+
+  return { name, rules };
+}
+
 function prepareInput(input) {
-  return input;
+  const [start, ...states] = input.trim().split("\n\n");
+
+  const [startStateLine, checksumStepsLine] = start.split("\n");
+  const startState = startStateLine.split(" ")[3].split(".")[0];
+  const checksumSteps = parseInt(checksumStepsLine.split(" ")[5]);
+
+  const transitions = {};
+  for (let i = 0; i < states.length; i++) {
+    const state = parseState(states[i]);
+    transitions[state.name] = state.rules;
+  }
+
+  return { startState, checksumSteps, transitions };
+}
+
+function step(state, cursor, tape, transitions) {
+  const value = tape[cursor] || 0;
+  const rule = transitions[state][value];
+
+  tape[cursor] = rule.write;
+  cursor += rule.move;
+  state = rule.state;
+
+  return [state, cursor, tape];
 }
 
 function part1(input) {
-    return input;
-}
+  const { startState, checksumSteps, transitions } = input;
 
-function part2(input) {
-    return input;
+  let state = startState;
+  let cursor = 0;
+  let tape = {};
+
+  for (let i = 0; i < checksumSteps; i++) {
+    [state, cursor, tape] = step(state, cursor, tape, transitions);
+  }
+
+  return Object.values(tape).reduce((acc, val) => acc + val, 0);
 }
 
 export default function solve(input) {
-    input = prepareInput(input);
-    return "Day25\nPart1: " + part1(input) + "\nPart2: " + part2(input) + "\n";
+  input = prepareInput(input);
+  return "Day25\nPart1: " + part1(input) + "\n";
 }
